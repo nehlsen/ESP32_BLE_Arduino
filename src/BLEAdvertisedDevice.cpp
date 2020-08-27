67f2603104b68bd2f8ae7f4d2ed3201782923b36
Nils Weigel <nils.weigel@gmail.com>
2020-08-27 09:28:39 +0200

    fixed stringbuffer size

--- src/BLEAdvertisedDevice.cpp
+++ src/BLEAdvertisedDevice.cpp
@@ -538,7 +538,7 @@ std::string BLEAdvertisedDevice::toString() {
 		}
 	}
 	if (haveTXPower()) {
-		char val[4];
+		char val[5];
 		snprintf(val, sizeof(val), "%d", getTXPower());
 		res += ", txPower: ";
 		res += val;
